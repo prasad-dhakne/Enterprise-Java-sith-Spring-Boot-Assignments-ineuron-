59499340368c339cc78332101c5346f75fb8302a
prasad2411 <dhakaneprasad@gmail.com>
2022-07-19 18:04:42 +0530

    assignment1/pattern1  added

--- src/core_java_assignments/Assignment1.java
+++ src/core_java_assignments/Assignment1.java
@@ -1,5 +1,16 @@
 package core_java_assignments;
 
 public class Assignment1 {
-
+	public static void main(String[] args) {
+		int num = 5;
+		pattern1(num);
+	}
+	public static void pattern1(int num) {
+		for(int i = 1; i <= num; i++) {
+			for(int j = 1; j <= i; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
 }
