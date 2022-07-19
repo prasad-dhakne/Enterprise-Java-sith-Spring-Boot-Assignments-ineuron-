eaf0c33b5ec8e763c6b6c9269d7cc0d5b81c98bb
prasad2411 <dhakaneprasad@gmail.com>
2022-07-19 18:24:12 +0530

    assignment/pattern2  added

--- src/core_java_assignments/Assignment1.java
+++ src/core_java_assignments/Assignment1.java
@@ -2,10 +2,24 @@ package core_java_assignments;
 
 public class Assignment1 {
 	public static void main(String[] args) {
+		System.out.println("Pattern 1: ");
 		int num = 5;
-		pattern1(num);
+		rightUpperTriangle(num);
+		System.out.println();
+		System.out.println("Pattern 2: ");
+		int row = 6;
+		int col = 3;
+		rectangle(row, col);
 	}
-	public static void pattern1(int num) {
+	public static void rectangle(int row, int col) {
+		for(int i = 1; i <= row; i++) {
+			for(int j = 1; j <= col; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+	public static void rightUpperTriangle(int num) {
 		for(int i = 1; i <= num; i++) {
 			for(int j = 1; j <= i; j++) {
 				System.out.print("* ");
